668f8b9895b07a610f19df6c32bedc7b5914d153
Peter Christian Agbenyega <peter.cagbenyega@gmail.com>
2026-03-05 03:27:55 -0500

    chore: add backend bootstrap + fix ec2/main outputs

---
+++ bootstrap/main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "peter-terraform-state-backend-001" # must be globally unique
+}
+
+resource "aws_s3_bucket_versioning" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "tf_locks" {
+  name         = "peter-terraform-state-locks-001"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.tf_state.bucket
+}
+
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.tf_locks.name
+}
--- main.tf
+++ main.tf
@@ -8,16 +8,13 @@ terraform {
     }
   }
 
-  # ----------------------------------------
-  # REMOTE STATE (S3) + STATE LOCKING (DynamoDB)
-  # NOTE: Backend does NOT support var.* or locals.*
-  # Replace the placeholders below with YOUR real values.
-  # ----------------------------------------
+  # Remote state (S3) + state locking (DynamoDB)
+  # Backend does NOT support var.* or locals.*
   backend "s3" {
-    bucket         = "REPLACE_ME-terraform-state-bucket"
-    key            = "REPLACE_ME/resilientops-cloud-architecture/terraform.tfstate"
+    bucket         = "peter-terraform-state-backend-001"
+    key            = "resilientops-cloud-architecture/dev/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "REPLACE_ME-terraform-lock-table"
+    dynamodb_table = "peter-terraform-state-locks-001"
     encrypt        = true
   }
 }
@@ -29,16 +26,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-
-terraform {
-  backend "s3" {
-    bucket         = "peter-terraform-state-backend-001"
-    key            = "resilientops-cloud-architecture/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "peter-terraform-state-locks-001"
-    encrypt        = true
-  }
-}
 # ----------------------------------------
 # VPC MODULE
 # ----------------------------------------
@@ -51,7 +38,6 @@ module "vpc" {
   private_cidr_blocks  = var.private_cidr_blocks
   database_cidr_blocks = var.database_cidr_blocks
 
-  # Class convention: tags passed into modules
   tags = local.merged_tags
 }
 
